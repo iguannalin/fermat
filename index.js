c93b8da8537c1f0f7a318e17a99da3b9da6d4904
seasons986 <annalin12345@gmail.com>
2023-12-01 01:12:57 -0500

    credit where credits due

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 window.addEventListener("load", () => {
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-  }
-  const container = document.getElementById("container");
   const button = document.getElementById("button");
   let int;
   let card = 0;
-  // viridis color palette from--https://waldyrious.net/viridis-palette-generator/
   let color = {h:50,s:72.73,l:12.94};//hsl(50deg 72.73% 12.94%);
 
+  // fermat algo from--https://craftofcoding.wordpress.com/2018/04/18/building-a-visualization-of-a-sunflower-spiral-ii/
   let angle = Math.PI*(3.0-Math.sqrt(5.0)); //137.5 in radians
   let r, theta;
   function drawCard() {
@@ -23,9 +17,6 @@ window.addEventListener("load", () => {
     // Convert polar to cartesian
     let x = r * Math.cos(theta) + 500// + dia;
     let y = r * Math.sin(theta) + 375// + dia;
-    
-    console.log({card,x,y});
-
     const text = `<!DOCTYPE html><html> <head> <title>${card+1}</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"><style>body{margin:0 auto;text-align:center;color: #f9faffde;
       text-shadow: #858ebc -2px 2px 8px;width:100vw;height:100vh;background-color:hsl(${color.h}deg ${color.s}% ${color.l}%);}</style><h1></h1></body></html>`;
     const blob = new Blob([text], {type: "text/html"});
